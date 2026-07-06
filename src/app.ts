095b59c2d74261e8efcc6042b81a0237315450ed
Md-Ramjan-Ali <gmramjanali8888@gmail.com>
2026-07-07 03:13:44 +0600

    feat: initialize Express application with base route handler

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import  express, { Application } from "express";
+import express, { Application } from "express";
 
-const app: Application = express()
+const app: Application = express();
 
-app.get('/', (req,res) => {
-  res.send("Welcome to the Prisma Blog App")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Prisma Blog App");
+});
 
-export default app
+export default app;
