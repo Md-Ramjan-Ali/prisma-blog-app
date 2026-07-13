d3ba896f9b038b8ca24fb2716a42162b4305bb25
ramjan-devs <gmramjanali100@gmail.com>
2026-07-13 21:08:52 +0600

    feat: initialize authentication system using better-auth and Prisma schema models

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 
 app.use(express.json());
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use("/", postRouter);
 
